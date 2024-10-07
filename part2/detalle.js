2b32d7e0750cc09676d14ce7937fbe3b60e641ac
carlostupac <102556656+carlostupac@users.noreply.github.com>
2024-10-07 10:45:25 -0300

    Perfil en menu desplegable
    Meter el perfil en el menú desplegable cuando la pantalla se hace chica.

--- part2/detalle.js
+++ part2/detalle.js
@@ -8,12 +8,36 @@ function validarCompra() {
     else{
         window.location.href = "Resumen_compra.html";
     }
+}
+
+
+function incluirNombre(){
+    w = window.innerWidth;
+    
+    a = document.getElementById("nav-adicional")
+    b = document.getElementById("pegar_1")
+    c = document.getElementById("nav-bar")
+    
+
+    if(500 > w){
+        console.log("es menor que 500")
+        b.appendChild(a)
+        a.removeAttribute("class")
+
     }
+    else if(500 <= w){
+        console.log("es mayor que 500")
+        c.appendChild(a)
+        a.setAttribute("class","nav-extra")
+    }
+}
 
 
 function cargar_eventos(){
 
-    /* aun por definirse */
-    document.getElementById("btn-comprar").addEventListener("click", validarCompra);
+    incluirNombre()
+    document.getElementById("button_prueba").addEventListener("click", validarCompra);
+    window.addEventListener("resize",incluirNombre);
+    
  
 }
