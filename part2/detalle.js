cb09ba04a259a008fecbfa7abc9f568d9bf6eb0d
Vanessa Holguin <vanessaholguin267@gmail.com>
2024-10-07 22:45:14 -0300

    apartado usuario dentro de menu
    para pantallas chicas, en todas las pantallas de la pagina. :)

--- part2/detalle.js
+++ part2/detalle.js
@@ -11,33 +11,9 @@ function validarCompra() {
 }
 
 
-function incluirNombre(){
-    w = window.innerWidth;
-    
-    a = document.getElementById("nav-adicional")
-    b = document.getElementById("pegar_1")
-    c = document.getElementById("nav-bar")
-    
-
-    if(500 > w){
-        console.log("es menor que 500")
-        b.appendChild(a)
-        a.removeAttribute("class")
-
-    }
-    else if(500 <= w){
-        console.log("es mayor que 500")
-        c.appendChild(a)
-        a.setAttribute("class","nav-extra")
-    }
-}
-
 
 function cargar_eventos(){
-
-    incluirNombre()
     document.getElementById("button_prueba").addEventListener("click", validarCompra);
-    window.addEventListener("resize",incluirNombre);
     
  
 }
