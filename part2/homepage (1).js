5bba9ceaad5ed0bf1af49792b32fb774b379f7f4
carlostupac <102556656+carlostupac@users.noreply.github.com>
2024-10-11 22:53:38 -0300

    Header
    Sigo peleando con el header

--- part2/homepage (1).js
+++ part2/homepage (1).js
@@ -1,37 +1,29 @@
 
 function openNav() {
     document.getElementById("nav1").style.width = "250px";
-    document.getElementById("nav1").style.height = "120%";
-    document.getElementById("mySidebar").style.height = "100%";
-    document.getElementById("mySidebar").style.overflow = "visible";
-    document.getElementById("barra-desplegable").style.display = "none";
-    
+    document.getElementById("nav1").style.height = "30%";
+
+    document.getElementById("mySidebar").style.display ="block";
+
+    document.getElementById("barra-desplegable").style.visibility = "hidden";
 }
 
 function closeNav() {
     document.getElementById("nav1").style.width = "0";
     document.getElementById("nav1").style.height = "0";
-    document.getElementById("mySidebar").style.height = "0";
-    document.getElementById("mySidebar").style.overflow = "hidden";
-    document.getElementById("barra-desplegable").style.display = "inline";
-    
-  
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("barra-desplegable").style.visibility = "visible";
 }
 
 
 function incluirNombre(){
     w = window.innerWidth;
-        
     a = document.getElementById("nav-adicional")
     b = document.getElementById("pegar_1")
     c = document.getElementById("nav-bar")
-    
-
     if(600 > w){
         console.log("es menor que 600")
         b.appendChild(a)
-        
-
     }
     else if(600 <= w){
         console.log("es mayor que 600")
@@ -42,9 +34,6 @@ function incluirNombre(){
 
 
 function cargar_eventos(){
-
     incluirNombre()
     window.addEventListener("resize",incluirNombre);
-    
- 
 }
