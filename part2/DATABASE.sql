fc139bb523ecf03bf20c25d5d123117b4a1ec858
Vanessa Holguin <vanessaholguin267@gmail.com>
2024-10-22 09:09:20 -0300

    database2

--- part2/DATABASE.sql
+++ part2/DATABASE.sql
@@ -1,5 +1,19 @@
+
 SHOW DATABASES;
-USE carlost_beerseba1;
+USE carlost_berseeva2;
 
 SHOW TABLES;
 
+SELECT * FROM USUARIOS;
+
+INSERT INTO USUARIOS (id, nombre, apellido, email, pass, tipo_usuario)
+    VALUES (NULL, 'MARTHA', 'REYES', 'martha24@gmail.com', '456789', 'cliente');
+
+SELECT * FROM Compra;
+INSERT INTO Compra(id, id_usuario, Fechahora, Estado)
+    VALUES (NULL, 1, 2024-10-16, );
+
+SELECT * FROM detallecompra;
+
+INSERT INTO (id, id_compra, id_producto, cantidad, total_compra)
+    VALUES (NULL, )
