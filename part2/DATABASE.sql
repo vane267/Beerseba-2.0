1d208a236a33aa0848503c471c8ab2566c336e90
Vanessa Holguin <vanessaholguin267@gmail.com>
2024-10-22 17:16:45 -0300

    base de datos y boton fav detalle

--- part2/DATABASE.sql
+++ part2/DATABASE.sql
@@ -7,13 +7,54 @@ SHOW TABLES;
 SELECT * FROM USUARIOS;
 
 INSERT INTO USUARIOS (id, nombre, apellido, email, pass, tipo_usuario)
-    VALUES (NULL, 'MARTHA', 'REYES', 'martha24@gmail.com', '456789', 'cliente');
+    VALUE (NULL, 'VALERIA', 'MASSA', 'VALEMASSITA@gmail.com', '123456', 'cliente'),
+    (NULL, 'JUAN', 'CASERES', 'CASITAJ@gmail.com', '123456', 'cliente'),
+    (NULL, 'MAELO', 'RUIZ', 'RUIZOM@gmail.com', '123456', 'cliente'),
+    (NULL, 'JUAN LUIS', 'GUERRA', 'BILIRRUBINA@gmail.com', '123456', 'cliente'),
+    (NULL, 'ENRIQUE', 'IGLESIAS', 'ENRIQUESITO@gmail.com', '123456', 'cliente');
+
+
+SELECT * FROM Tipo_Producto;
+INSERT INTO Tipo_Producto (id, tipo)
+    VALUE (NULL, 'Vestido'),
+    (NULL, 'Pantalon'),
+    (NULL, 'Remera'),
+    (NULL, 'Short'),
+    (NULL, 'Blazer');
+
+SELECT * FROM Producto;
+
+INSERT INTO Producto (id, nombre, id_tipo_producto, img)
+    VALUE (NULL, 'Vestido de Lino', '1', 'url'),
+    (NULL, 'Vestido de Fibrana', '1', 'url'),
+    (NULL, 'Vestido de Morley', '1', 'url'),
+    (NULL, 'Pantalon de Bengalina', '2', 'url'),
+    (NULL, 'Blazer de Crep', '5', 'url');
+
+SELECT * FROM Producto_Talle;
+INSERT INTO Producto_Talle (id, id_tipo_producto, Talle, precio, stock, descripción)
+    VALUE (NULL, 3, '10', 15000.00, 200, 'Remera de Morley') ;
+
+    
+INNER JOIN 
 
 SELECT * FROM Compra;
 INSERT INTO Compra(id, id_usuario, Fechahora, Estado)
-    VALUES (NULL, 1, 2024-10-16, );
+    VALUE (NULL, 1, '2024-10-16', 'En proceso'),
+        (NULL, 2, '2024-8-16', 'Entregado'),
+        (NULL, 3, '2024-10-16', 'Cancelado'),
+        (NULL, 4, '2024-10-20', 'En proceso');
 
-SELECT * FROM detallecompra;
 
-INSERT INTO (id, id_compra, id_producto, cantidad, total_compra)
-    VALUES (NULL, )
+SELECT * FROM Compra_Detalle;
+
+INSERT INTO Compra_Detalle (id, id_compra, id_producto_talle, cantidad, precio)
+    VALUE (NULL, 1, 16, 2, 30000);
+
+
+SELECT USUARIO.nombre AS nombre,
+    Producto.nombre AS Producto,
+    Compra.fechahora AS Fecha, 
+    Compra_Detalle.cantidad AS Cantidad,
+    Compra_Detalle.precio AS Total 
+
